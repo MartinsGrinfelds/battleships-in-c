9ad02195876ae2b5db466e8cdef923af28682d88
Mārtiņš Kristaps Mickus <64271878+MartinsKMickus@users.noreply.github.com>
2024-09-28 18:14:13 +0300

    Player username handling
    Closes  BIC-61, closes BIC-62, closes BIC-63, closes BIC-60, closes BIC-36. Adds and refactors some data structures. Adds ACK packet serialization/deserialization. Checks user input and doesn't let two same usernames.

--- packets/shared_packets.c
+++ packets/shared_packets.c
@@ -3,37 +3,92 @@
 #include <stdlib.h> // exit(), size_t, malloc
 #include <stdio.h> // printf
 
+// IMPORTANT!!! SERIALIZATION IS NOT NEEDED IF PACKET CONTAINS PURE VALUES.
+// Packet serialization/deserialization helps to put information together in one chunk.
+// Without this deserialized packets contain information about fields.
+// Fields can contain address to another memory and normally sending the packet we would not send data but only address.
+// It is required to read multiple address contents into one chunk of data.
+// To make this working you have to deserialize in same order as you serialized the content.
+
 char *hello_packet_serialization(struct HelloPacket *packet, size_t *final_size)
 {
     *final_size = sizeof(packet->name_length) + packet->name_length;
     // Call free(address) for this one or enjoy memory leak 😍
-    char* decompressed_packet = malloc(*final_size);
-    char *filler_pointer = decompressed_packet;
+    char* serialized_packet = malloc(*final_size);
+    char *filler_pointer = serialized_packet;
     *(uint8_t *)filler_pointer = packet->name_length;
     filler_pointer += sizeof(uint8_t);
     char *name_pointer = packet->name;
-    while ((size_t)filler_pointer - (size_t)decompressed_packet < *final_size)
+    while ((size_t)filler_pointer - (size_t)serialized_packet < *final_size)
     {
         *filler_pointer++ = *name_pointer++;
     }
-    return decompressed_packet;
+    return serialized_packet;
 }
 
 void hello_packet_deserialization(char *serialized_packet, struct HelloPacket *deserialized_packet)
 {
     char *read_pointer = serialized_packet;
-    deserialized_packet->name_length = *(uint8_t *)read_pointer;
+    deserialized_packet->name_length = *(uint8_t *)read_pointer++;
     
-    deserialized_packet->name = malloc(deserialized_packet->name_length + 1); // +1 is \0
-    read_pointer++;
+    deserialized_packet->name = malloc(deserialized_packet->name_length + 1); // +1 is for \0 character
     char *write_pointer = deserialized_packet->name;
-    while (read_pointer - serialized_packet <= deserialized_packet->name_length)
+    char* name_start_pointer = read_pointer;
+    // read_pointer points at first name character.
+    // By subtracting name start pointer we can know processed name length.
+    while (read_pointer - name_start_pointer < deserialized_packet->name_length)
     {
         *write_pointer++ = *read_pointer++;
     }
-    write_pointer = '\0'; // For possibility to use printf()
+    *write_pointer = '\0'; // Fill last name character with \0 or risk hoping it was 0
     return;
 }
 
+char *ack_packet_serialization(struct AckPacket *packet, size_t *final_size)
+{
+    *final_size = sizeof(packet->team_id) + sizeof(packet->player_id) + sizeof(packet->message_length) + packet->message_length;
+    // Call free(address) for this one or enjoy memory leak 😍
+    char* serialized_packet = malloc(*final_size);
+    char *filler_pointer = serialized_packet;
+    // Player ID
+    *(uint8_t *)filler_pointer = packet->player_id;
+    filler_pointer += sizeof(uint8_t);
+    // Team ID
+    *(uint8_t *)filler_pointer = packet->team_id;
+    filler_pointer += sizeof(uint8_t);
+    // Message length
+    *(uint8_t *)filler_pointer = packet->message_length;
+    filler_pointer += sizeof(uint8_t);
+    // Message
+    char *message_pointer = packet->message;
+    while ((size_t)filler_pointer - (size_t)serialized_packet < *final_size)
+    {
+        *filler_pointer++ = *message_pointer++;
+    }
+    return serialized_packet;
+}
+
+void ack_packet_deserialization(char *serialized_packet, struct AckPacket *deserialized_packet)
+{
+    char *read_pointer = serialized_packet;
+    // Player ID
+    deserialized_packet->player_id = *(uint8_t *)read_pointer++;
+    // Team ID
+    deserialized_packet->team_id = *(uint8_t *)read_pointer++;
+    // Message length
+    deserialized_packet->message_length = *(uint8_t *)read_pointer++;
+    char* message_start_pointer = read_pointer;
+    // Message
+    deserialized_packet->message = malloc(deserialized_packet->message_length + 1); // +1 is for \0 character
+    char *write_pointer = deserialized_packet->message;
+    // read_pointer points at first message character.
+    // By subtracting message start pointer we can know processed message length.
+    while (read_pointer - message_start_pointer < deserialized_packet->message_length)
+    {
+        *write_pointer++ = *read_pointer++;
+    }
+    *write_pointer = '\0'; // Fill last message character with \0 or risk hoping it was 0
+    return;
+}
 
 // TODO: Add other packet serialization/deserialization
--- packets/shared_packets.h
+++ packets/shared_packets.h
@@ -2,40 +2,46 @@
 #include <stdint.h> // uint definitions
 #include <stdlib.h> // exit(), size_t, malloc
 
-// Max username length
-#define MAX_USERNAME 30
+// Min/Max username length
+#define MAX_USERNAME 20
+#define MIN_USERNAME 1
 
 
 /// @brief DATA-0020
 struct Player
 {
-    uint8_t player_id;
+    int socket_nr;
+    uint8_t id;
     uint8_t team_id;
-    uint8_t player_status;
-    uint8_t player_name_length;
-    char *player_name;
+    uint8_t status;
+    char username[MAX_USERNAME];
 };
 
+/// @brief DATA-0021
 struct GenericPacket
 {
     uint32_t sequence_number;     // 4 bytes
-    uint32_t packet_content_size; // 4 bytes
+    size_t packet_content_size; // ? bytes
     uint8_t packet_type;          // 1 byte
     uint8_t checksum;             // 1 byte
     // Padding 6 bytes here <- Free space to use
     char *content; // Pointer is 8? bytes Content is saved somewhere on memory
 };
 
+/// @brief DATA-0022
 struct HelloPacket
 {
     uint8_t name_length;
     char *name;
 };
 
+/// @brief DATA-0023
 struct AckPacket
 {
     uint8_t player_id;
     uint8_t team_id;
+    uint8_t message_length;
+    char* message;
 };
 
 
@@ -49,3 +55,14 @@ char *hello_packet_serialization(struct HelloPacket *packet, size_t *final_size)
 /// @param serialized_packet Pointer to serialized Hello packet.
 /// @param deserialized_packet Pointer to Hello packet where to put data in.
 void hello_packet_deserialization(char *serialized_packet, struct HelloPacket *deserialized_packet);
+
+/// @brief Takes ACK packet and makes one long chunk of data so it can be used within GenericPacket.
+/// @param packet Pointer to ACK packet.
+/// @param final_size Pointer to serialization size after serialization.
+/// @return Pointer to a serialized packet. FREE AFTER USE!!!
+char *ack_packet_serialization(struct AckPacket *packet, size_t *final_size);
+
+/// @brief Takes serialized ACK packet and deserializes it.
+/// @param serialized_packet Pointer to serialized ACK packet.
+/// @param deserialized_packet Pointer to ACK packet where to put data in.
+void ack_packet_deserialization(char *serialized_packet, struct AckPacket *deserialized_packet);
